21f78b40d106235261b6e38280c01f9cc9fa1594
CattonA <alexcatton48@gmail.com>
2022-02-23 22:40:34 +0000

    updated footer code

--- src/global-components/Footer.jsx
+++ src/global-components/Footer.jsx
@@ -4,8 +4,14 @@ function Footer() {
   return (
 
     <footer>
-        <div class = "wrapper"> 
-        SudentBee &copy 2022
+        <div class = "footer-main"> 
+            <Link to = '/login'> Sign in</Link>
+            <Link to = "/#"> Accomodation</Link>
+            
+        
+        </div>
+        <div class = "footer-bottom">
+            StudentBee &copy; 2022
         </div>
     </footer>
   )
