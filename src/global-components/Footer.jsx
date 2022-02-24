3346509178195fb3574f333de9d7b98a522fe44b
Marlin Ranasinghe <dinethmarlinranasinghe@gmail.com>
2022-02-24 11:42:13 +0000

    Imported link tag and corrected className

--- src/global-components/Footer.jsx
+++ src/global-components/Footer.jsx
@@ -1,20 +1,17 @@
+import { Link } from 'react-router-dom';
 
+import styles from './Footer.module.css';
 
 function Footer() {
   return (
-
-    <footer>
-        <div class = "footer-main"> 
-            <Link to = '/login'> Sign in</Link>
-            <Link to = "/#"> Accomodation</Link>
-            
-        
-        </div>
-        <div class = "footer-bottom">
-            StudentBee &copy; 2022
-        </div>
+    <footer className={styles.footer}>
+      <div classNames={styles.footerMain}>
+        <Link to='/login'>Sign in</Link>
+        <Link to='/#'>Accommodation</Link>
+      </div>
+      <div class='footer-bottom'>StudentBee &copy; 2022</div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
