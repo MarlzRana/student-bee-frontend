03c7f1b731d7512e68c88543976a0737f87b7c54
MarkCTantoy <mctproject@outlook.com>
2022-02-28 18:57:57 +0000

    Description box for widget added

--- src/sections/events-page/components/FloatingWidget.jsx
+++ src/sections/events-page/components/FloatingWidget.jsx
@@ -6,20 +6,11 @@ function FloatingWidget(props) {
     <div class={styles.floatingWidget}>
         <div class={styles.contents}>
           <img src={image} alt = "Event Image"/>
-          {/* <div class={styles.description}>
-            Placeholder Text
-          </div> */}
+          <div class={styles.description}>
+            <h1>Music Fesitval</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+          </div>
         </div>
-        {/* <div>0</div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div> */}
-        {/* <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div> */}
     </div>
   )
 }
