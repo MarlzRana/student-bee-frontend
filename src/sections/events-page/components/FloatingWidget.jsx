7e13076785c330b8600b68af2f6175aae23625d8
MarkCTantoy <mctproject@outlook.com>
2022-03-02 20:00:48 +0000

    Image now passed in as prop

--- src/sections/events-page/components/FloatingWidget.jsx
+++ src/sections/events-page/components/FloatingWidget.jsx
@@ -1,14 +1,14 @@
 import styles from "../styling/FloatingWidget.module.css";
-import image from '../styling/placeholder.jpg';
+// import image from '../styling/placeholder.jpg';
 
 function FloatingWidget(props) {
-  const {eventName, description} = props;
+  const {eventName, description, image} = props;
   return (
     <div class={styles.floatingWidget}>
       <img src={image} alt = "Event Image"/>
-        <div class={styles.description}>
-          <h1>{props.eventName}</h1>
-          <p>{props.description}</p>
+        <div class={styles.description} style={{backgroundImage: '../styling/placeholder.jpg'}}>
+          <h1>{eventName}</h1>
+          <p>{description}</p>
           <a className={styles.btn} href="#" >Find out more</a>
         </div>
     </div>
