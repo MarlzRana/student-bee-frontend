2bbd272f7ed5d733523833bb6332962cbba7efbb
MarkCTantoy <mctproject@outlook.com>
2022-03-05 17:10:39 +0000

    Elements of widget gathered

--- src/sections/events-page/Events.jsx
+++ src/sections/events-page/Events.jsx
@@ -9,10 +9,18 @@ function Events() {
   return (
     <div className={styles.events}>
       {/* <Suspense fallback={<div>Loading...</div>}>
-            <FloatingWidget eventName="Music Event" description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a " image={img} />
-        </Suspense> */}
+        <FloatingWidget
+          eventName="Music Event"
+          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a "
+          image={img}
+        />
+      </Suspense> */}
       <Suspense fallback={<div>Loading...</div>}>
-        <YellowWidget eventName="Cosplay Party" eventDate="23/03/2022" />
+        <YellowWidget
+          eventName="Cosplay Party"
+          eventDate="23/03/2022"
+          image={img}
+        />
       </Suspense>
     </div>
   );
