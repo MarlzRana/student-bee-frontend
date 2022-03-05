c872ce05157ba40f609e3aa07a141497464a28c9
MarkCTantoy <mctproject@outlook.com>
2022-03-05 17:34:06 +0000

    Size of widget set to 75% of floating widget

--- src/sections/events-page/Events.jsx
+++ src/sections/events-page/Events.jsx
@@ -8,13 +8,13 @@ const YellowWidget = lazy(() => import("./components/YellowWidget"));
 function Events() {
   return (
     <div className={styles.events}>
-      {/* <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <FloatingWidget
           eventName="Music Event"
           description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a "
           image={img}
         />
-      </Suspense> */}
+      </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
         <YellowWidget
           eventName="Cosplay Party"
--- src/sections/events-page/components/YellowWidget.jsx
+++ src/sections/events-page/components/YellowWidget.jsx
@@ -1,21 +1,16 @@
 import styles from "../styling/YellowWidget.module.css";
-// import image from '../styling/placeholder.jpg';
-
-// const widgetStyle = {
-//   backgroundImage: image,
-// };
 
 function FloatingWidget(props) {
   const { eventName, eventDate, image } = props;
   return (
     <div class={styles.yellowWidget}>
       <img src={image} alt="Event Image" />
-      {/* <div class={styles.description}>
+      <div class={styles.description}>
         <h1>{eventName}</h1>
         <p>{eventDate}</p>
-      </div> */}
-      <h1>{eventName}</h1>
-      <p>{eventDate}</p>
+      </div>
+      {/* <h1>{eventName}</h1>
+      <p>{eventDate}</p> */}
     </div>
   );
 }
