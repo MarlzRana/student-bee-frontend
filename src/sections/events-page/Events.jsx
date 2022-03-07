89992d780de1734bcbb9dc191a2fa5df8f89039b
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-07 10:25:33 +0000

    Made the widget spinner thingy

--- src/sections/events-page/Events.jsx
+++ src/sections/events-page/Events.jsx
@@ -7,9 +7,27 @@ const FloatingWidget = lazy(() => import('./components/FloatingWidget'));
 function Events() {
   return (
     <div className={styles.events}>
-        <Suspense fallback={<div>Loading...</div>}>
-            <FloatingWidget eventName="Music Event" description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a " image={img} />
-        </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className={styles.container}>
+          <div className={styles.spinner}>
+              <span className={styles.first}>
+                <div className='eventWidget'>
+                  <FloatingWidget eventName="Music Event" description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a " image={img} />
+                </div>  
+              </span> 
+              <span className={styles.second}>
+                <div className='eventWidget'>
+                  <FloatingWidget eventName="Music Event" description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a " image={img} />
+                </div>  
+              </span> 
+              <span className={styles.third}>
+                <div className='eventWidget'>
+                  <FloatingWidget eventName="Music Event" description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sgaittis dictum mauris, eget tincidunt nulla dignissim ut. Quisque cursus ante placerat, auctor tellus vitae, accumsan neque. Etiam a tincidunt lorem. Nam magna erat, posuere vitae viverra nec, mollis sit amet libero. as da sda sd ad asd a " image={img} />
+                </div>  
+              </span> 
+          </div>  
+        </div>
+      </Suspense>  
     </div>
   )
 }
