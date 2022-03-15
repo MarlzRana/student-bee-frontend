4e8ba4bb1a14845780dc16f69f2a17e2d8588f9d
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-15 17:24:37 +0000

    Creation of register and login page

--- src/sections/login-page/components/LoginWidget.jsx
+++ src/sections/login-page/components/LoginWidget.jsx
@@ -1,6 +1,6 @@
 import styles from '../styling/LoginWidget.module.css';
 
-function LoginWidget() {
+function LoginWidget({ reg, setReg }) {
   return (
     <div className={styles.widget}>
       <header className={styles.header}>
@@ -32,6 +32,7 @@ function LoginWidget() {
           <p className= {styles.incorrect}>Placeholder for incorrect username/password</p>
         </div>
         <div className={styles.submit}>
+          <button className={styles.back} onClick={() => setReg(true)} ontype="button">Back</button>
           <input
           className={styles.submitButton}
           type="submit"
