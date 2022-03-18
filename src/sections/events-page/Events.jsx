b1348546b27bbed2637ac83641969e9e3d61c7b7
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-18 03:37:18 +0000

    Importing components in Events jsx

--- src/sections/events-page/Events.jsx
+++ src/sections/events-page/Events.jsx
@@ -1,8 +1,17 @@
 import { Suspense, lazy } from "react";
 import styles from "./Events.module.css";
 
+const AddEventWidget = (lazy => import('./components/AddEventWidget'));
+const FloatingWidget = (lazy => import('./components/FloatingWidget'));
+const WidgetSpinner = (lazy => import('./components/WidgetSpinner'));
+const YellowWidget = (lazy => import('./components/YellowWidget'));
+
 function Events() {
-  return <div></div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      test
+    </Suspense>
+  );
 }
 
 export default Events;
