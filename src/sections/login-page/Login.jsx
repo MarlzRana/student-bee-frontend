e314f3d18dd750174b4174d227b0f7a2ab5cebce
Marlin Ranasinghe <dinethmarlinranasinghe@gmail.com>
2022-03-19 11:17:06 +0000

    Fixed the structure for login page

--- src/sections/login-page/Login.jsx
+++ src/sections/login-page/Login.jsx
@@ -3,23 +3,24 @@ import styles from './Login.module.css';
 import { lazy, Suspense, useState } from 'react';
 
 const LoginWidget = lazy(() => import('./components/LoginWidget'));
-const RegisterWidget = lazy(() => import("../register-widget/RegisterWidget"));
-const HomePageNavbar = lazy(() =>
-  import("../home-page/components/Navbar")
-);
+const RegisterWidget = lazy(() => import('./components/RegisterWidget'));
+const HomePageNavbar = lazy(() => import('../home-page/components/Navbar'));
 
 function Login() {
-
-  const [register, setRegister] = useState(true)
+  const [register, setRegister] = useState(true);
 
   return (
     <body>
       <Suspense fallback={<div>Loading...</div>}>
-        <HomePageNavbar/>
+        <HomePageNavbar />
         <div className={styles.border}></div>
         <div className={styles.widget}>
-          {register ? (<RegisterWidget reg={register} setReg={setRegister}/>) : (<LoginWidget reg={register} setReg={setRegister}/>)}
-        </div>  
+          {register ? (
+            <RegisterWidget reg={register} setReg={setRegister} />
+          ) : (
+            <LoginWidget reg={register} setReg={setRegister} />
+          )}
+        </div>
         <div className={styles.botBorder}></div>
       </Suspense>
     </body>
---
+++ src/sections/login-page/components/RegisterWidget.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import styles from '../styling/RegisterWidget.module.css';
+
+function RegisterWidget({ reg, setReg }) {
+  return (
+    <div className={styles.registerWidget}>
+      <header className={styles.header}>
+        <h1>Join the hive</h1>
+      </header>
+      <form className={styles.form}>
+        <div className={styles.row0}>
+          <div className={styles.formTextInput}>
+            <input
+              type='text'
+              placeholder='First Name'
+              name='FirstName'
+              id='FirstName'
+              required
+            />
+          </div>
+          <div className={styles.horizontalSpacer}></div>
+          <div className={styles.formTextInput}>
+            <input
+              type='text'
+              placeholder='Last Name'
+              name='LastName'
+              id='LastName'
+              required
+            />
+          </div>
+        </div>
+        <div className={styles.formTextInput}>
+          <input
+            type='text'
+            placeholder='Email'
+            name='email'
+            id='email'
+            required
+          />
+        </div>
+        <div className={styles.formTextInput}>
+          <input
+            type='text'
+            placeholder='Username'
+            name='username'
+            id='username'
+            required
+          />
+        </div>
+        <div className={styles.formTextInput}>
+          <input
+            type='password'
+            placeholder='Password'
+            name='psw'
+            id='psw'
+            required
+          />
+        </div>
+        <div className={styles.formTextInput}>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='psw-confirm'
+            id='psw-confirm'
+            required
+          />
+        </div>
+        <div className={styles.formDateInput}>
+          <label htmlFor='dob'>Date of Birth:</label>
+          <input type='date' name='psw-confirm' id='psw-confirm' required />
+        </div>
+        <div className={styles.errorMessage}>
+          <p>That username already exists!</p>
+        </div>
+        <div className={styles.submit}>
+          <input type='submit' name='submit' value='Register' />
+          <button onClick={() => setReg(false)} ontype='button'>
+            Already have an account?
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterWidget;
