40f6a9ee8dfe2e7859b3bf0b814c551b9bf34aee
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-23 01:03:33 +0000

    Created add society widget and its styling

--- src/sections/societies-page/Societies.jsx
+++ src/sections/societies-page/Societies.jsx
@@ -3,37 +3,50 @@ import { Suspense, lazy, useState } from 'react';
 import img from './styling/placeholder2.jpg';
 
 const Society = lazy(() => import('./components/Society'));
+const AddSocietyWidget = lazy(() => import('./components/AddSocietyWidget'));
 
 function Societies() {
     const [isAddSocietyWidgetShowing, setIsAddSocietyWidgetShowing] = useState(false);
     return(
-        <div className={styles.societiesPage}>
-            <div className={styles.societiesHeading}>
-                <h1>Societies</h1>
-                <p>Spice up your university life!</p>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className={styles.societiesPage}>
+                <div className={styles.societiesHeading}>
+                    <h1>Societies</h1>
+                    <p>Spice up your university life!</p>
+                </div>
+                <div className={styles.societiesContainer}>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                    <Society societyName="Gaming" societyImage={img}/>
+                </div>
+                {isAddSocietyWidgetShowing ? (
+                <AddSocietyWidget
+                    setIsAddSocietyWidgetShowing={setIsAddSocietyWidgetShowing}
+                />
+                ) : (
+                    <></>
+                )}
+                {!isAddSocietyWidgetShowing ? (<button
+                    title='Add your own society!'
+                    onClick={() => setIsAddSocietyWidgetShowing(true)}
+                    className={styles.addSociety}
+                >
+                    +
+                </button>
+                ) : (
+                    <></>
+                )}
             </div>
-            <div className={styles.societiesContainer}>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-                <Society societyName="Gaming" societyImage={img}/>
-            </div>
-            <button
-                title='Add your own society!'
-                onClick={() => setIsAddSocietyWidgetShowing(true)}
-                className={styles.addSociety}
-            >
-                +
-            </button>
-        </div>
+        </Suspense>    
     );
 }
 
---
+++ src/sections/societies-page/components/AddSocietyWidget.jsx
@@ -0,0 +1,77 @@
+import { React, useState } from 'react';
+import styles from '../styling/AddSocietyWidget.module.css';
+
+function AddSocietyWidget({ setIsAddSocietyWidgetShowing }) {
+  return (
+    <div className={styles.background}>
+      <div className={styles.addEventsWidget}>
+        <header className={styles.header}>
+          <h1>Society Details</h1>
+        </header>
+        <form className={styles.form}>
+          <div className={styles.row0}>
+            <div className={styles.formTextInput}>
+              <input
+                type='text'
+                placeholder='Society Name'
+                name='SocietyName'
+                id='SocietyName'
+                required
+              />
+            </div>
+          </div>
+          <div className={styles.formTextInput}>
+            <input
+              type='text'
+              placeholder='Username (Society Leader)'
+              name='username'
+              id='enteredLeaderName'
+              required
+            />
+          </div>
+          <div className={styles.formTextInput}>
+            <input
+              type='text'
+              placeholder='Name (Society Leader)'
+              name='name'
+              id='enteredLeaderName'
+              required
+            />
+          </div>
+          <div className={styles.formTextInput}>
+            <input
+              type='text'
+              placeholder='Link'
+              name='link'
+              id='enteredLink'
+              required
+            />
+          </div>
+          <div className={styles.formTextInput}>
+            <textarea
+              placeholder='Society Description'
+              name='desc'
+              id='enteredDescription'
+              required
+            />
+          </div>
+          <div className={styles.submit}>
+            <input
+              type='submit'
+              name='submit'
+              value='Add'
+            />
+            <button onClick={() => setIsAddSocietyWidgetShowing(false)}>
+              Cancel
+            </button>
+          </div>
+        </form>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddSocietyWidget;
