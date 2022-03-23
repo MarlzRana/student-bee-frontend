9668aa7999ba236bc12bfad49f9e390e7009ca21
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-23 00:11:32 +0000

    Replaced society page bg and fixed a few bugs

--- src/sections/societies-page/Societies.jsx
+++ src/sections/societies-page/Societies.jsx
@@ -1,27 +1,38 @@
 import styles from './Societies.module.css';
-import { Suspense, lazy } from 'react';
-import img from './styling/placeholder.jpg';
+import { Suspense, lazy, useState } from 'react';
+import img from './styling/placeholder2.jpg';
 
 const Society = lazy(() => import('./components/Society'));
 
 function Societies() {
+    const [isAddSocietyWidgetShowing, setIsAddSocietyWidgetShowing] = useState(false);
     return(
         <div className={styles.societiesPage}>
             <div className={styles.societiesHeading}>
                 <h1>Societies</h1>
                 <p>Spice up your university life!</p>
             </div>
-
             <div className={styles.societiesContainer}>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
-                <Society societyName="coomers" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
+                <Society societyName="Gaming" societyImage={img}/>
             </div>
+            <button
+                title='Add your own society!'
+                onClick={() => setIsAddSocietyWidgetShowing(true)}
+                className={styles.addSociety}
+            >
+                +
+            </button>
         </div>
     );
 }
--- src/sections/societies-page/components/Society.jsx
+++ src/sections/societies-page/components/Society.jsx
@@ -1,10 +1,13 @@
 import styles from '../Societies.module.css';
+import { Link } from 'react-router-dom';
 
 function Society({ societyName, societyImage }) {
     return (
         <div className={styles.societiesCell}>
-            <img src={societyImage} alt={societyName} className={styles.societiesImage}/>
-            <div className={styles.societiesText}>{societyName}</div>
+            <Link className={styles.societyRedirect} to='/mainApp/societyDetails'>
+                <img src={societyImage} alt={societyName} className={styles.societiesImage}/>
+                <div className={styles.societiesText}>{societyName}</div>
+            </Link>
         </div>
     );
 }
