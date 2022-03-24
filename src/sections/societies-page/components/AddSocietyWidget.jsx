0710f8a76d3672deb35811fae5ff832ef4cb291b
MarkCTantoy <mctproject@outlook.com>
2022-03-24 14:36:21 +0000

    Injected backend logic. User can now add events
    Co-authored-by: Rax <MrRax786@users.noreply.github.com>

--- src/sections/societies-page/components/AddSocietyWidget.jsx
+++ src/sections/societies-page/components/AddSocietyWidget.jsx
@@ -1,7 +1,41 @@
-import { React, useState } from 'react';
-import styles from '../styling/AddSocietyWidget.module.css';
+import { React, useState } from "react";
+import Axios from "axios";
+import styles from "../styling/AddSocietyWidget.module.css";
 
 function AddSocietyWidget({ setIsAddSocietyWidgetShowing }) {
+  const [enteredSocietyName, setEnteredSocietyName] = useState("");
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredLeaderName, setEnteredLeaderName] = useState("");
+  const [enteredLink, setEnteredLink] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const addSociety = async (e) => {
+    e.preventDefault();
+    const payload = {
+      societyNameIn: enteredSocietyName,
+      usernameIn: enteredUsername,
+      societyLeaderNameIn: enteredLeaderName,
+      societyMainSocialLinkIn: enteredLink,
+      societyDescriptionIn: enteredDescription,
+    };
+    console.log(payload);
+    try {
+      const res = await Axios.post(
+        process.env.REACT_APP_APIHOSTADDRESS + "/societiesSystem/add",
+        payload
+      );
+      if (res.data.status === "success") {
+        console.log(res);
+        console.log("success");
+      }
+      if (res.data.status === "failure") {
+        console.log(res);
+        console.log("failure");
+      }
+    } catch (error) {
+      window.confirm("Something went wrong. Please try again later.");
+    }
+  };
+
   return (
     <div className={styles.background}>
       <div className={styles.addEventsWidget}>
@@ -12,54 +46,60 @@ function AddSocietyWidget({ setIsAddSocietyWidgetShowing }) {
           <div className={styles.row0}>
             <div className={styles.formTextInput}>
               <input
-                type='text'
-                placeholder='Society Name'
-                name='SocietyName'
-                id='SocietyName'
+                type="text"
+                placeholder="Society Name"
+                name="enteredSocietyName"
+                id="SocietyName"
+                onChange={(e) => setEnteredSocietyName(e.target.value)}
                 required
               />
             </div>
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Username (Society Leader)'
-              name='username'
-              id='enteredLeaderName'
+              type="text"
+              placeholder="Username (Society Leader)"
+              name="enteredUsername"
+              id="enteredLeaderName"
+              onChange={(e) => setEnteredUsername(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Name (Society Leader)'
-              name='name'
-              id='enteredLeaderName'
+              type="text"
+              placeholder="Name (Society Leader)"
+              name="enteredLeaderName"
+              id="enteredLeaderName"
+              onChange={(e) => setEnteredLeaderName(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Link (Discord, Instagram, etc)'
-              name='link'
-              id='enteredLink'
+              type="text"
+              placeholder="Link (Discord, Instagram, etc)"
+              name="enteredLink"
+              id="enteredLink"
+              onChange={(e) => setEnteredLink(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <textarea
-              placeholder='Society Description'
-              name='desc'
-              id='enteredDescription'
+              placeholder="Society Description"
+              name="enteredDescription"
+              id="enteredDescription"
+              onChange={(e) => setEnteredDescription(e.target.value)}
               required
             />
           </div>
           <div className={styles.submit}>
             <input
-              type='submit'
-              name='submit'
-              value='Add'
+              type="submit"
+              name="submit"
+              value="Add"
+              onClick={(e) => addSociety(e)}
             />
             <button onClick={() => setIsAddSocietyWidgetShowing(false)}>
               Cancel
