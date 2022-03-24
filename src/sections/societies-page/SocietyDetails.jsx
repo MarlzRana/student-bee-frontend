30761fc6a06cf97dad03c498a750079fa3861514
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-24 11:41:30 +0000

    Created the societyMember in a separate component

--- src/sections/societies-page/SocietyDetails.jsx
+++ src/sections/societies-page/SocietyDetails.jsx
@@ -1,6 +1,8 @@
 import styles from './SocietyDetails.module.css';
-import { Suspense } from 'react';
 import pic from '../profile-section/components/DefaultProfilePic.jpg';
+import { Suspense, lazy } from 'react';
+
+const SocietyMember = lazy(() => import('./components/SocietyMember'));
 
 function SocietyDetails() {
     return(
@@ -12,34 +14,28 @@ function SocietyDetails() {
                     </div>  
                 </div> 
                 <div className={styles.societyMembers}>
-                            <h2>Members</h2>
-                            <ul>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                                <li>
-                                    <img src={pic} alt="User's profile pic" />
-                                    <span className={styles.userLabel}>First Last</span>
-                                </li>
-                            </ul>
-                        </div>
+                    <h2>Members</h2>
+                    <ul>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                        <li>
+                            <SocietyMember profilePic={pic} firstName='First' lastName='Last'/>
+                        </li>
+                    </ul>
+                </div>
                 <div className={styles.societyDesc}>
                     <div className={styles.societyInfo}>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet erat porttitor, imperdiet nunc a, tincidunt magna. Phasellus quis scelerisque lacus, sit amet ultricies nisi. Cras commodo purus bibendum dolor dictum, et viverra lacus eleifend. Nulla facilisi. Maecenas eu libero eget tortor lacinia dictum vitae id urna. Aenean porttitor pulvinar dui non mattis. Curabitur hendrerit lectus at ornare rhoncus. Duis a est augue. Sed elit mauris, semper et sapien at, placerat aliquam orci.
