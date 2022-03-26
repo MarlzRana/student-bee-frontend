abe4d69107023ba86eb2e10a7957d9c65298c912
MarkCTantoy <mctproject@outlook.com>
2022-03-26 14:51:33 +0000

    User can now add job
    Co-authored-by: CattonA <CattonA@users.noreply.github.com>
    Co-authored-by: Rax <MrRax786@users.noreply.github.com>

--- src/sections/jobs-page/components/AddJobWidget.jsx
+++ src/sections/jobs-page/components/AddJobWidget.jsx
@@ -1,7 +1,59 @@
-import { React } from 'react';
-import styles from '../styling/AddJobWidget.module.css';
+import { React, useState } from "react";
+import styles from "../styling/AddJobWidget.module.css";
+import Axios from "axios";
 
 function AddJobWidget({ setIsAddJobWidgetShowing }) {
+  const [enteredJobName, setEnteredJobName] = useState("");
+  const [enteredWage, setEnteredWage] = useState("");
+  const [enteredHours, setEnteredHours] = useState("");
+  const [enteredLocation, setEnteredLocation] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredContactNumber, setEnteredContactNumber] = useState("");
+  const [enteredContactEmail, setEnteredContactEmail] = useState("");
+  const [enteredApplicationLink, setEnteredApplicationLink] = useState("");
+  const [enteredJobDescription, setEnteredJobDescription] = useState("");
+  const [messageToShow, setMessageToShow] = useState("");
+
+  const addJob = async (e) => {
+    e.preventDefault();
+    const payload = {
+      jobName: enteredJobName,
+      wage: enteredWage,
+      workingHours: enteredHours,
+      location: enteredLocation,
+      startDate: enteredDate,
+      description: enteredJobDescription,
+      employerEmail: enteredContactEmail,
+      employerPhoneNumber: enteredContactNumber,
+      link: enteredApplicationLink,
+    };
+    try {
+      const res = await Axios.post(
+        process.env.REACT_APP_APIHOSTADDRESS + "/jobsSystem/addJob",
+        payload
+      );
+      if (res.data.status === "success") {
+        setMessageToShow("Success! Your job has been added");
+        setEnteredJobName("");
+        setEnteredWage("");
+        setEnteredHours("");
+        setEnteredDate("");
+        setEnteredContactNumber("");
+        setEnteredContactEmail("");
+        setEnteredApplicationLink("");
+        setEnteredJobDescription("");
+      }
+      if (res.data.status === "failure") {
+        if (res.data.reason === "invalidInputFormat") {
+          console.log(res);
+          setMessageToShow("Invalid inputs");
+        }
+      }
+    } catch (error) {
+      window.confirm("Something went wrong. Please try again later.");
+    }
+  };
+
   return (
     <div className={styles.background}>
       <div className={styles.addJobWidget}>
@@ -12,91 +64,101 @@ function AddJobWidget({ setIsAddJobWidgetShowing }) {
           <div className={styles.row0}>
             <div className={styles.formTextInput}>
               <input
-                type='text'
-                placeholder='Job Name'
-                name='JobName'
-                id='JobName'
+                type="text"
+                placeholder="Job Name"
+                name="enteredJobName"
+                id="JobName"
+                onChange={(e) => setEnteredJobName(e.target.value)}
                 required
               />
             </div>
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Wage'
-              name='wage'
-              id='wage'
+              type="text"
+              placeholder="Wage"
+              name="enteredWage"
+              id="wage"
+              onChange={(e) => setEnteredWage(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Working hours per week'
-              name='hoursPerWeek'
-              id='hoursPerWeek'
+              type="text"
+              placeholder="Working hours per week"
+              name="enteredHours"
+              id="hoursPerWeek"
+              onChange={(e) => setEnteredHours(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Location'
-              name='location'
-              id='location'
+              type="text"
+              placeholder="Location"
+              name="enteredLocation"
+              id="location"
+              onChange={(e) => setEnteredLocation(e.target.value)}
               required
             />
           </div>
           <div className={styles.formDateInput}>
             <label htmlFor="startDate">Start Date</label>
             <input
-              type='date'
-              placeholder='Start Date'
-              name='startDate'
-              id='startDate'
+              type="date"
+              placeholder="Start Date"
+              name="enteredDate"
+              id="startDate"
+              onChange={(e) => setEnteredDate(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Employer Contact Number'
-              name='contactNo'
-              id='contactNo'
+              type="text"
+              placeholder="Employer Contact Number"
+              name="enteredContactNumber"
+              id="contactNo"
+              onChange={(e) => setEnteredContactNumber(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Employer Contact Email'
-              name='contactEmail'
-              id='contactEmail'
+              type="text"
+              placeholder="Employer Contact Email"
+              name="enteredContactEmail"
+              id="contactEmail"
+              onChange={(e) => setEnteredContactEmail(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <input
-              type='text'
-              placeholder='Application Link'
-              name='appLink'
-              id='appLink'
+              type="text"
+              placeholder="Application Link"
+              name="enteredApplicationLink"
+              id="appLink"
+              onChange={(e) => setEnteredApplicationLink(e.target.value)}
               required
             />
           </div>
           <div className={styles.formTextInput}>
             <textarea
-              placeholder='Job Description and Requirements'
-              name='desc'
-              id='enteredDesc'
+              placeholder="Job Description and Requirements"
+              name="enteredJobDescription"
+              id="enteredDesc"
+              onChange={(e) => setEnteredJobDescription(e.target.value)}
               required
             />
           </div>
           <div className={styles.submit}>
             <input
-              type='submit'
-              name='submit'
-              value='Add'
+              type="submit"
+              name="submit"
+              value="Add"
+              onClick={(e) => addJob(e)}
             />
             <button onClick={() => setIsAddJobWidgetShowing(false)}>
               Cancel
@@ -104,7 +166,7 @@ function AddJobWidget({ setIsAddJobWidgetShowing }) {
           </div>
         </form>
         <div>
-          <p></p>
+          <p>{messageToShow}</p>
         </div>
       </div>
     </div>
