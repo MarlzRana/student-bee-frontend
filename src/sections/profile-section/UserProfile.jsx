8ec14c2696593484a4bdeb51d5aefa0f1c8365e2
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-28 01:44:49 +0100

    Finished user profile page

--- src/sections/profile-section/UserProfile.jsx
+++ src/sections/profile-section/UserProfile.jsx
@@ -13,8 +13,19 @@ function UserProfile() {
                     <p>1st Year, Computer Science</p>
                     <br />
                     <div className={styles.userDescription}>
-                        <h2>Description</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin purus at odio vehicula consectetur. Donec elementum a erat a maximus. Sed id dolor id urna porttitor elementum. Sed eu nisi at lectus dignissim varius. Donec sit amet dignissim risus. Fusce imperdiet ullamcorper diam vel sodales. Donec erat orci, facilisis a turpis ac, blandit tempus tellus. Duis congue purus id purus maximus cursus.</p>
+                        <h2>About Me</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin purus at odio vehicula consectetur. Donec elementum a erat a maximus. Sed id dolor id urna porttitor elementum. Sed eu nisi at lectus dignissim varius. Donec sit amet dignissim risus. Fusce imperdiet ullamcorper diam vel sodales. Donec erat orci, facilisis a turpis ac, blandit tempus tellus. Duis congue purus id purus maximus cursus.
+                        </p>                    
+                    </div>
+                    <br />
+                    <div className={styles.joinedSocieties}>
+                        <h2>Societies</h2>
+                        <ul>
+                            <li>Gaming</li>
+                            <li>Computer Science</li>
+                            <li>Aerospace</li>
+                        </ul>
                     </div>
                 </div>
             </div>
