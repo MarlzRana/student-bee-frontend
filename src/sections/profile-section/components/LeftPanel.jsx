64a4780d602301688b0515d1a389c305bf97abc8
MarkCTantoy <mctproject@outlook.com>
2022-03-28 15:37:17 +0100

    The current user's details are displayed on home

--- src/sections/profile-section/components/LeftPanel.jsx
+++ src/sections/profile-section/components/LeftPanel.jsx
@@ -2,20 +2,22 @@ import styles from "../styling/LeftPanel.module.css";
 import profilePic from "./DefaultProfilePic.jpg";
 import { Link } from "react-router-dom";
 
-function LeftPanel() {
+function LeftPanel({ firstName, lastName, courseName }) {
   return (
     <div className={styles.leftPanel}>
       <div className={styles.panelContents}>
         <img src={profilePic} alt="profilePic" />
-        <h3>Profile Name</h3>
-        <p>Course</p>
-        <Link className={styles.btn} to='/mainApp/editProfile'>
+        <h3>
+          {firstName} {lastName}
+        </h3>
+        <p>{courseName}</p>
+        <Link className={styles.btn} to="/mainApp/editProfile">
           Edit Profile
         </Link>
-        <Link className={styles.btn} to='/mainApp/mySocieties'>
+        <Link className={styles.btn} to="/mainApp/mySocieties">
           My Societies
         </Link>
-        <Link className={styles.btn} to='/mainApp/myEvents'>
+        <Link className={styles.btn} to="/mainApp/myEvents">
           My Events
         </Link>
       </div>
