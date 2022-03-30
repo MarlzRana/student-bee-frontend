7ea33af46979fe8469011a0c85554d4403897490
Akmal Rizal <akmal.reezal@gmail.com>
2022-03-30 12:16:48 +0100

    Fixed search router

--- src/sections/societies-page/components/Search.jsx
+++ src/sections/societies-page/components/Search.jsx
@@ -1,6 +1,9 @@
 import styles from "../styling/Search.module.css";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Search() {
+    const [address, setAddress] = useState("/mainApp/societySearch");
     return (
         <div className={styles.searchContainer}>
             <form className={styles.searchBar}>
@@ -10,14 +13,17 @@ function Search() {
                     name="search"
                     type="search"
                     placeholder="Search..."
+                    onChange={(e) => setAddress("/mainApp/societySearch/" + e.target.value)}
                     required
                 />
-                <button 
-                    className={styles.searchButton} 
-                    type="submit"
-                >
-                    <i className={styles.searchIcon}></i>
-                </button>
+                <Link to={address}>
+                    <button 
+                        className={styles.searchButton} 
+                        type="submit"
+                    >
+                        <i className={styles.searchIcon}></i>
+                    </button>
+                </Link>
             </form>
         </div>    
     );
